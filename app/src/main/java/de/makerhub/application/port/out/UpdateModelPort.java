35edab9bf0ae193fcaa4ba230975faaad7945b5c
Herbermann, Sebastian <acetous@users.noreply.github.com>
2024-12-30 18:01:45 +0100

    Refactor hexagonal architecture * apply package structure proposed by Tom Hombergs

---
+++ app/src/main/java/de/makerhub/application/port/out/UpdateModelPort.java
@@ -0,0 +1,7 @@
+package de.makerhub.application.port.out;
+
+import de.makerhub.domain.Model;
+
+public interface UpdateModelPort {
+    void updateModel(Model model);
+}
--- app/src/main/java/de/makerhub/port/UpdateModelPort.java
+++
@@ -1,7 +0,0 @@
-package de.makerhub.port;
-
-import de.makerhub.domain.model.Model;
-
-public interface UpdateModelPort {
-    void updateModel(Model model);
-}
